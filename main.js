1866bd2ee300c7c0f916a298158ac318bc542665
Daniel Mihovch <dmihovch@gmail.com>
2026-03-22 01:21:58 -0400

    it is, technically, a website

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function hello() {
-  console.log("You suck at programming!");
-}
+let num_pets = 0;
+const pets_counter = document.getElementById("num-pets")
+document.getElementById("pet-btn").addEventListener("click",addPet)
 
-document.getElementById("ysap").addEventListener("click", hello);
+
+
+function addPet()
+{
+	num_pets++;
+	pets_counter.textContent = num_pets;
+
+}
